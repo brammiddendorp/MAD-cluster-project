070dc52dbf0232a23a6c008a4b1158d888e40c94
130948 <71711096+130948@users.noreply.github.com>
2022-03-07 12:47:47 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 MASTER=$1
 DOMAIN=$2
+ADMIN_PASS=$3
 
 # PREPARATION
 sudo yum install -y ansible git pwgen
@@ -8,23 +9,25 @@ sudo yum install -y ansible git pwgen
 # BASIS
 # Get playbook
 git clone https://github.com/spantaleev/matrix-docker-ansible-deploy.git ~/matrix-docker-ansible-deploy
-mkdir ~/matrix-docker-ansible-deploy/inventory/host_vars/matrix1.$DOMAIN
-cp ~/matrix-docker-ansible-deploy/examples/vars.yml ~/matrix-docker-ansible-deploy/inventory/host_vars/matrix1.$DOMAIN/vars.yml
+mkdir ~/matrix-docker-ansible-deploy/inventory/host_vars/matrix.$DOMAIN
+cp ~/matrix-docker-ansible-deploy/examples/vars.yml ~/matrix-docker-ansible-deploy/inventory/host_vars/matrix.$DOMAIN/vars.yml
 # Set domain
-sed -i 's/YOUR_BARE_DOMAIN_NAME_HERE/'$DOMAIN'/' ~/matrix-docker-ansible-deploy/inventory/host_vars/matrix1.$DOMAIN/vars.yml
+sed -i 's/YOUR_BARE_DOMAIN_NAME_HERE/'$DOMAIN'/' ~/matrix-docker-ansible-deploy/inventory/host_vars/matrix.$DOMAIN/vars.yml
 # Set homeserver secret
-sed -i -e "s/matrix_homeserver_generic_secret_key: ''/matrix_homeserver_generic_secret_key: '$(pwgen -s 64 1)'/" ~/matrix-docker-ansible-deploy/inventory/host_vars/matrix1.$DOMAIN/vars.yml
+sed -i -e "s/matrix_homeserver_generic_secret_key: ''/matrix_homeserver_generic_secret_key: '$(pwgen -s 64 1)'/" ~/matrix-docker-ansible-deploy/inventory/host_vars/matrix.$DOMAIN/vars.yml
 # Set lets encrypt email
-sed -i "s/matrix_ssl_lets_encrypt_support_email: ''/matrix_ssl_lets_encrypt_support_email: 'webmaster@$DOMAIN'/" ~/matrix-docker-ansible-deploy/inventory/host_vars/matrix1.$DOMAIN/vars.yml
+sed -i "s/matrix_ssl_lets_encrypt_support_email: ''/matrix_ssl_lets_encrypt_support_email: 'webmaster@$DOMAIN'/" ~/matrix-docker-ansible-deploy/inventory/host_vars/matrix.$DOMAIN/vars.yml
 # Set postgres password
-sed -i -e "s/matrix_postgres_connection_password: ''/matrix_postgres_connection_password: '$(pwgen -s 64 1)'/" ~/matrix-docker-ansible-deploy/inventory/host_vars/matrix1.$DOMAIN/vars.yml
+sed -i -e "s/matrix_postgres_connection_password: ''/matrix_postgres_connection_password: '$(pwgen -s 64 1)'/" ~/matrix-docker-ansible-deploy/inventory/host_vars/matrix.$DOMAIN/vars.yml
 
 cp ~/matrix-docker-ansible-deploy/examples/hosts ~/matrix-docker-ansible-deploy/inventory/hosts
 # set server domain
-sed -i 's/matrix.<your-domain>/matrix1.'$DOMAIN'/' ~/matrix-docker-ansible-deploy/inventory/hosts
+sed -i 's/matrix.<your-domain>/matrix.'$DOMAIN'/' ~/matrix-docker-ansible-deploy/inventory/hosts
 # set server IP
 sed -i "s/<your-server's external IP address>/$MASTER/" ~/matrix-docker-ansible-deploy/inventory/hosts
 
 # ACTIVATE
 #install + start
 ansible-playbook -i ~/matrix-docker-ansible-deploy/inventory/hosts ~/matrix-docker-ansible-deploy/setup.yml --tags=setup-all,start
+#register admin
+ansible-playbook -i ~/matrix-docker-ansible-deploy/inventory/hosts ~/matrix-docker-ansible-deploy/setup.yml --extra-vars='username=admin password='$ADMIN_PASS' admin=yes' --tags=register-user
